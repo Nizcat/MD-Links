bd2295b4e6589407d4d22d4f9bd08ee81e9dbc78
Nizcat <nizalli@gmail.com>
2022-04-18 18:36:59 -0500

    antes de una función

--- index.js
+++ index.js
@@ -3,13 +3,25 @@ const path = require('path');
 
 
 module.exports = function mdLinks(ruta, callback) {
-    fs.readdir(ruta, (err, list) => {
-        if (err) {
-            return callback(err, null);
-        } else {
-            callback(null, list)
-        }
-    })
+    if (path.extname(ruta) == ".md") {
+        fs.readFile(ruta, (err, list) => {
+            console.log("tiene.md");
+            if (err) {
+                return callback(err, null);
+            } else {
+                return callback(null, list)
+            }
+        })
+    } else if (path.extname(ruta) == "") {
+        console.log(ruta, "es carpeta");
+        fs.readdir(ruta, (err, list) => {
+            if (err) {
+                return callback(err, null);
+            } else {
+                console.log(list, "viendoarchivos");
+                return callback(null, list)
+            }
+        })
+    }
 }
-
 
--- module.js
+++ module.js
@@ -1,14 +1,50 @@
 const mdLinks = require('./index.js')
 const path = require('path');
 
+let mdFiles = []
+let dir = []
+
+function carpetas(dir) {
+    console.log(dir, "carpetas a abrir");
+    dir.forEach(element =>  mdLinks(path.resolve(element), function (err, files) {
+        console.log(path.resolve(element), "antes del if");
+        if (err) {
+           console.log(err, "desde carpetas, ruta de archivo incorrecta");
+        } else {
+            files.forEach(file => {     
+                if (path.extname(file) == ".md"){
+                   mdFiles.push(path.resolve(file)); 
+                   } else if (path.extname(file) == "" && file[0] != "."){
+                    dir.push(path.resolve(file)); 
+                   }
+                }); 
+        }
+    }))
+    }
+    
+
 mdLinks(process.argv[2], function (err, files) {
+    console.log(process.argv[2], "porqué esta ruta no?");
     if (err) {
-       console.log(err, "hay un error en el proceso");
+       console.log(err, "desde mdlinks, ruta de archivo incorrecta");
     } else {
-        files.forEach(file => {
-        if (path.extname(file) == ".md") { 
-        console.log(path.join(__dirname, file));
-    };
-})  
-}
-})
+        files.forEach(file => {    
+            if (path.extname(file) == ".md"){
+                console.log(file, "soy la lectura de un archivo md?"); 
+               mdFiles.push(path.resolve(file)); 
+               } else if (path.extname(file) == ""&& file[0] != "."){
+                dir.push(file);
+                 
+               }
+            }); 
+            carpetas(dir);
+    console.log(mdFiles, "archivos listos para leer md");
+    }
+}) 
+
+
+/* const carpetasRutaActual = (path.join(__dirname, (path.dirname(process.argv[2])))); */
+/* console.log(carpetasRutaActual); */
+
+
+
