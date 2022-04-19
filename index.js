9dc63a88d069d692db79892ce90ef5d1348c1cc7
Nizcat <nizalli@gmail.com>
2022-04-19 07:03:30 -0500

    readFiles

--- index.js
+++ index.js
@@ -24,4 +24,24 @@ module.exports = function mdLinks(ruta, callback) {
         })
     }
 }
+module.exports = function carpetas(dir) {
+    console.log(dir, "carpetas a abrir");
+    dir.forEach(element => mdLinks(path.resolve(element), function (err, files) {
+        console.log(path.resolve(element), "antes del if");
+        if (err) {
+            console.log(err, "desde carpetas, ruta de archivo incorrecta");
+        } else {
+            files.forEach(file => {
+                if (path.extname(file) == ".md") {
+                    mdFiles.push(path.resolve(file));
+                } else if (path.extname(file) == "" && file[0] != ".") {
+                    dir.push(path.resolve(file));
+                }
+            });
+        }
+    }))
+    carpetas(dir, "segunda vuelta");
+    reading(mdFiles, "segunda vuelta");
+
+}
 
