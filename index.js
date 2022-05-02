e5dc2272d6f84aba69fb5197b71fdd7ec8e390f8
Nizcat <nizalli@gmail.com>
2022-05-02 12:14:29 -0500

    promesa devuelve array con validate true y false

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const mdLinks = require('./utils/md-links.js')
 
+const options = {
+    validate : "true",
+}
 
-mdLinks(process.argv[2]);
+mdLinks(process.argv[2], options.validate).then((thing) =>{
+    console.log(thing, "enindex");
+})
  
     
--- utils/md-links.js
+++ utils/md-links.js
@@ -1,19 +1,21 @@
+const { rejects } = require('assert');
 const fs = require('fs');
 const path = require('path');
 const readingFile = require('./readFile.js')
 const recursive = require('./recursive.js')
 const readUrl = require('./validate.js')
 
-module.exports = function mdLinks(ruta) {
-
-  if (path.extname(ruta) == ".md") {
-    console.log(readUrl(readingFile(path.resolve(ruta))), "enmdlinks");
-
-  } else {
-    recursive(path.resolve(ruta)).forEach(element => {
-      mdLinks(path.resolve(path.join(ruta, element)));
-    });
-  }
+module.exports = function mdLinks(ruta, opt) {
+  return new Promise((resolve, reject) => {
+    if (path.extname(ruta) == ".md" && opt === "false") {
+      resolve(readingFile(path.resolve(ruta)));
+    } else if (path.extname(ruta) == ".md" && opt === "true") {
+      resolve(readUrl(readingFile(path.resolve(ruta))))
+    } else {
+      recursive(path.resolve(ruta)).forEach(element => {
+        mdLinks(path.resolve(path.join(ruta, element)));
+      });
+    }
+  })
 }
-
 
