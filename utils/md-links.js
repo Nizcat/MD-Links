376880768ceb06c32f6039c778e2a0474fcb9086
Nizcat <nizalli@gmail.com>
2022-05-04 12:45:23 -0500

    regresa stats y validate al 100% bin

--- utils/md-links.js
+++ utils/md-links.js
@@ -6,22 +6,35 @@ const recursive = require('./recursive.js')
 const readUrl = require('./validate.js')
 let container = []
 
-module.exports = function mdLinks(ruta, opt) {
+
+
+module.exports = function mdLinks(ruta, opt, stat) {
   return new Promise((resolve, reject) => {
-    if (path.extname(ruta) == ".md" && opt === "false") {
-      resolve(readingFile(path.resolve(ruta)));
-    } else if (path.extname(ruta) == ".md" && opt === "true") {
-      const mdArray =readingFile(path.resolve(ruta))
+    if (path.extname(ruta) == ".md" && opt === false && stat === false) {
+      resolve(readingFile(path.resolve(ruta), opt, stat));
+    } else if (path.extname(ruta) == ".md" && opt === false && stat === true) {
+      resolve(readingFile((path.resolve(ruta)), opt, stat))
+    } else if (path.extname(ruta) == ".md" && opt === true && stat === false) {
+      resolve(readUrl(readingFile(path.resolve(ruta), opt, stat),opt, stat));
+    } else if (path.extname(ruta) == ".md" && opt === true && stat === true) {
+      resolve(readUrl(readingFile(path.resolve(ruta), opt, stat),opt, stat));
+    } else if (path.extname(ruta) == ".md" && opt === true) {
+      const mdArray = readingFile(path.resolve(ruta))
       resolve(readUrl(mdArray))
-    } else if (fs.lstatSync(ruta).isDirectory() === true && opt === "false") {
-      resolve (readingFile(recursive(path.resolve(ruta))))
-    } else if (fs.lstatSync(ruta).isDirectory() === true && opt === "true") {
-      const otherArray = readingFile(recursive(path.resolve(ruta)))
-      resolve(readUrl(otherArray));
-     /*  resolve (readUrl(readingFile(recursive(path.resolve(ruta), container)))) */
+    } else if (fs.lstatSync(ruta).isDirectory() === true && opt === false && stat === false) {
+      resolve(readingFile(recursive(path.resolve(ruta)), opt, stat))
+    } else if (fs.lstatSync(ruta).isDirectory() === true && opt === false && stat === true) {
+      resolve(readingFile(recursive(path.resolve(ruta)), opt, stat))
+    } else if (fs.lstatSync(ruta).isDirectory() === true && opt === true && stat === true) {
+      const otherArray = readingFile(recursive(path.resolve(ruta)), opt, stat)
+      resolve(readUrl(otherArray, opt, stat));
+    } else if (fs.lstatSync(ruta).isDirectory() === true && opt === true && stat === false) {
+      const otherArray = readingFile(recursive(path.resolve(ruta), opt, stat))
+      resolve(readUrl(otherArray, opt, stat));
+     
     } else {
       reject("ERROR")
-  }
+    }
   })
 }
 
--- utils/validate.js
+++ utils/validate.js
@@ -1,40 +1,136 @@
 const https = require('https');
 const http = require('http');
 var request = require('sync-request');
-let failedUrls = []
-let okUrls = []
+const { url } = require('inspector');
 
-module.exports = function readUrl(urlPath) {
-    console.log("Espera un momento, por favor");
-    let text = "";
-let i = 0;
-do {
-    let res = request('GET', urlPath[i].href);
-    /* console.log('statusCode:', res.statusCode, urlPath[i].href); */
-    if (res.statusCode > 399) {
-        urlPath[i].status = res.statusCode;
-        urlPath[i].ok = "fail";
-        failedUrls.push(urlPath[i].href)
-    } else {
-        urlPath[i].status = res.statusCode;
-        urlPath[i].ok = "ok";
-        okUrls.push(urlPath[i].href)
-    }
- 
-  i++;
+const okUrls = []
+const brokenUrls = []
+
+function unique(urlArrays) {
+    let unique = [...new Set(urlArrays.map(item => item.href))];
+    return (unique.length)
 }
-while (i < urlPath.length);
-return urlPath;
-console.log(failedUrls.length, "fallidos");
-console.log(okUrls.length, "ok");
-   
-  /*   var res = request('GET', urlPath.href);
-    console.log('statusCode:', res.statusCode, urlPath.href);
-    if (res.statusCode > 399) {
-        failedUrls.push(urlPath.href)
+
+module.exports = function readUrl(urlPath, opt, stat) {
+    const arrayUrls = []
+    console.log("Espera un momento, por favor");
+
+    if (opt === false && stat === false) {
+        let text = "";
+        let i = 0;
+        do {
+            let res = request('GET', urlPath[i].href);
+            /* console.log('statusCode:', res.statusCode, urlPath[i].href); */
+            if (res.statusCode > 399) {
+                urlPath[i].status = res.statusCode;
+                urlPath[i].ok = "fail";
+                failedUrls.push(urlPath[i].href)
+            } else {
+                urlPath[i].status = res.statusCode;
+                urlPath[i].ok = "ok";
+                okUrls.push(urlPath[i].href)
+            }
+            /*  urlPath.Broken = failedUrls.length;
+             urlPath.Unique = okUrls.length;
+           */
+            i++;
+        }
+        while (i < urlPath.length);
+        /* console.log("Broken:",failedUrls.length);
+        console.log("Unique:", okUrls.length); */
+        return urlPath;
+
+    } else if (opt === true && stat === false) {
+        let text = "";
+        let i = 0;
+        do {
+            let res = request('GET', urlPath[i].href);
+            if (res.statusCode > 399) {
+                urlPath[i].status = res.statusCode;
+                urlPath[i].ok = "fail";
+            } else {
+                urlPath[i].status = res.statusCode;
+                urlPath[i].ok = "ok";
+                okUrls.push(urlPath[i].href)
+            }
+            const eachValidatedUrl = {
+                "href":urlPath[i].href,
+                "text":urlPath[i].text,
+                "file":urlPath[i].file,
+                "status": urlPath[i].status,
+                "ok":urlPath[i].ok,
+            }
+            arrayUrls.push(eachValidatedUrl)
+            i++;
+        }
+        while (i < urlPath.length);
+        return arrayUrls
+    } else if (opt === true && stat === true) {
+        let text = "";
+        let i = 0;
+        do {
+            let res = request('GET', urlPath[i].href);
+            if (res.statusCode > 399) {
+                urlPath[i].status = res.statusCode;
+                urlPath[i].ok = "fail";
+                brokenUrls.push(urlPath[i].href)
+            } else {
+                urlPath[i].status = res.statusCode;
+                urlPath[i].ok = "ok";
+                okUrls.push(urlPath[i].href)
+            }
+
+            i++;
+        }
+        while (i < urlPath.length);
+        const statsValidate = {
+            "Total": urlPath.length,
+            "Unique": unique(urlPath),
+            "Broken": brokenUrls.length,
+        }
+        return statsValidate
     } else {
-        okUrls.push(urlPath.href)
+        let text = "";
+        let i = 0;
+        do {
+            let res = request('GET', urlPath[i].href);
+            if (res.statusCode > 399) {
+                urlPath[i].status = res.statusCode;
+                urlPath[i].ok = "fail";
+            } else {
+                urlPath[i].status = res.statusCode;
+                urlPath[i].ok = "ok";
+                okUrls.push(urlPath[i].href)
+            }
+            arrayUrls.push("file:", urlPath[i].file, urlPath[i].href, res.statusCode, urlPath[i].ok)
+            i++;
+        }
+        while (i < urlPath.length);
+        const statsValidate = {
+            "Total": urlPath.length,
+            "Unique": unique(urlPath),
+            "Broken": brokenUrls.length,
+        }
+        return statsValidate
     }
-    console.log(failedUrls.length, "fallidos");
-    console.log(okUrls.length, "ok"); */
+
+
+
+}
+
+const validation = (link) => {
+
+    return axios.get(link.href)
+        .then(response => {
+            link.status = response.status;
+            link.ok = 'ok';
+            return link;
+        })
+        .catch(e => {
+            if (e.response) {
+                link.status = e.response.status;
+                link.ok = 'fail';
+                return link;
+            }
+        })
 }
